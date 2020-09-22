e0b4b68b9ab62b1e55020ba834ea99e156f105ad
jeongha-joo <64945491+jeongha-joo@users.noreply.github.com>
2020-09-22 15:27:41 +0900

    0922_룰렛

--- main.c
+++ main.c
@@ -1,41 +1,50 @@
 /*
- * GPIO_3.c
+ * ExInterrupt3.c
  *
- * Created: 2020-09-15 오후 1:13:20
+ * Created: 2020-09-22 오후 2:46:28
  * Author : IT학교
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
 
-void gpio_init(void)
-{
-	DDRC = 0xFF;
-	DDRE |= 0x00; //(0<<PE4) | (0<<PE5) | (0<<PE6) | (0<<PE7)
-	PINE = 0x00;
-	PORTC = 0xFF;	//led off
+//unsigned int sw_flag = 0;
+unsigned char ledData = 1;
+
+ISR(INT5_vect){
+	//sw_flag = !sw_flag;
+	ledData = ledData << 1;
+	PORTC = ~ledData;
+	if(0x80 == ledData)
+		ledData = 0x01;
+		_delay_ms(100);
 }
+
+void led_init(){
+	DDRC = 0xff;
+	PORTC = 0xff;
+}
+
+void swInterrupt_init(){
+	//DDRE(pe5-int5) 입력용 설정
+	DDRE = (0<<PE5);
+	// 외부인터럽트 int5 설정
+	EIMSK = (1 << INT5);
+	// 검출한 신호 설정 (low level)
+	EICRB = (0 << ISC51) | (0 << ISC50);
+	// 모든 외부인터럽트 발생을 초기화
+	EIFR = 0x00; //0 << INTF5;
+}
+
 int main(void)
 {
-	unsigned char swState=0;
-    gpio_init();
-    while (1) 
+    led_init();
+	swInterrupt_init();
+	sei();		//전역 인터럽트 set
+	while (1) 
     {
-		//swState = PINE;
-		swState = (PINE >> 4);
-		switch(swState){
-			case 0x0E : 
-				PORTC = 0x00;
-			break;
-			case 0x0D :
-				PORTC = 0xFF;
-			break;
-			case 0x0B :
-				PORTC = 0x00;
-			break;
-			case 0x07 :
-				PORTC = 0xFF;
-			break;
-		}
-    }
+		;
+	}
 }
 
